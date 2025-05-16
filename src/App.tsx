26d07819148ce047ff01b072cb205a42c26d4955
Pratham8487 <prathamshah8487@gmail.com>
2025-05-16 10:34:07 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (<div className="relative flex items-center justify-center h-40 w-40 border border-red-500">
+    <h1>This is React application.</h1>
+  </div>
+  );
+}
+
+export default App;
